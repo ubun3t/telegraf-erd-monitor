7a63e063ee9df9cf227617f547f65631efa71116
ubun3t <ubun3t@gmail.com>
2020-05-18 20:16:33 +0200

    Add files via upload

---
+++ check_erd_node_metrics_0.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OUTPUT=`curl -s 127.0.0.1:8080/node/status 2>/dev/null | jq ".details // empty"` # returns "" when null  
+  
+ret=$?
+if [ -z "${OUTPUT}" ] || [ ${ret} -ne 0 ]; then
+   echo "NODE NOT RUNNING!!"
+   exit 2 
+fi
+echo ${OUTPUT}
